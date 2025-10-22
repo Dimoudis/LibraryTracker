cfa93671091b128b6a5037cfa08a4884346396d0
Dimitris <98609381+Dimoudis@users.noreply.github.com>
2025-10-22 17:18:27 +0300

    Add files via upload

--- MovieTracker/tracker.py
+++ MovieTracker/tracker.py
@@ -4,24 +4,54 @@ import json
 library = []
 
 #add book in library
-def add_book(title, category, rating):
-    book = {
+def add_to_library (title, category, rating):
+    for item in library: 
+        if item["title"].lower() == title.lower():
+            print("Υπάρχει ήδη στη βιβλιοθήκη.")
+            return #σταματαει την εκτελεση της add_book()
+        
+    item = {
          "title":title,
          "category":category,
          "rating":rating
     }
     #add book
-    library.append(book)
+    library.append(item)
     #save in file
     save_library()  
     print(title + " προστέθηκε!")
 
 #user input 
 def user_input():
-    title_input = input("Τίτλος: ")
-    category_input = input("Κατηγορία (book/movie): ")
-    rating_input = int(input("Βαθμολογία (1-10): "))
-    add_book(title_input, category_input, rating_input)
+
+    #title
+    while True:
+        title_input = input("Τίτλος: ").strip()
+        if title_input != "":  #αν δεν ειναι κενός
+            break
+        else: 
+            print("Ο τιτλος δεν μπορεί να είναι κενός. Δοκίμασε ξανά.")
+
+    #category
+    while True:
+        category_input = input("Κατηγορία (book/movie): ").strip().lower()
+        if category_input in ["book","movie"]:
+            break
+        else: 
+            print("Πληκτρολόγησε μόνο 'book' ή 'movie'.")
+
+    #rating
+    while True:
+        try:
+            rating_input = int(input("Βαθμολογία (1-10): "))
+            if 1 <= rating_input <= 10:
+                break
+            else: 
+                print("Η βαθμολογία πρέπει να είναι μεταξύ 1 και 10.")
+        except ValueError:
+            print("Πρέπει να πληκτρολογήσεις έναν αριθμό (1-10).")
+    
+    add_to_library(title_input, category_input, rating_input)
  
 #show library
 def show_library():
@@ -29,8 +59,8 @@ def show_library():
         print("Η βιβλιοθήκη είναι άδεια!")
         return
     # Απλή εκτύπωση
-    for book in library:
-        print("Title:",book["title"],"- Category:", book["category"], "- Rating:", book["rating"])
+    for item in library:
+        print("Title:",item["title"],"- Category:", item["category"], "- Rating:", item["rating"])
 
 #load library se Json
 def load_library():
@@ -42,12 +72,31 @@ def load_library():
     except FileNotFoundError:
         library = []
         print("Δεν βρέθηκε αρχείο, ξεκινάμε νέα βιβλιοθήκη.")
+    except json.JSONDecodeError:
+        library = []
+        print("Το αρχείο library.json είναι κατεστραμμένο. Δημιουργείται νέο αρχείο.")
+        
 
 #save library se Json
 def save_library():
     with open("library.json", "w", encoding="utf-8") as f:
         json.dump(library, f, ensure_ascii=False, indent=4)
     print("Η βιβλιοθήκη αποθηκεύτηκε στο αρχείο.")
+
+
+#delete 
+def delete_from_library():
+
+    delete_title = input("Δωσε τον τίτλο του στοιχείου που θέλεις να διαγράψεις: ").strip()
+    for item in library:
+        if item["title"].lower() == delete_title.lower():
+            library.remove(item)
+            save_library()  # για να ενημερώνεται και το JSON
+            print("Το στοιχείο διαγράφηκε με επιτυχία!.")
+            break
+    else: 
+        print("Δεν υπάρχει στοιχείο με τίτλο", delete_title, "στην βιβλιοθήκη")
+        return
 
 #main
 def main():
@@ -57,7 +106,8 @@ def main():
         print("\nΤι θέλεις να κάνεις;")
         print("1 - Προσθήκη βιβλίου")
         print("2 - Προβολή βιβλιοθήκης")
-        print("3 - Έξοδος")
+        print("3 - Διαγραφή στοιχείου")
+        print("4 - Έξοδος")
 
         choice = input("Επέλεξε: ")
 
@@ -65,13 +115,13 @@ def main():
             user_input()
         elif choice == "2":
             show_library()
-            break
         elif choice == "3":
+            delete_from_library()
+        elif choice == "4":
             print("Έξοδος από το πρόγραμμα.")
             break
         else:
             print("Μη έγκυρη επιλογή, δοκίμασε ξανά.")
-            break
 
 if __name__ == "__main__": 
     main()
